fca3013fd166a161745449a3022b107839f61130
Ngazetungue <nmuheuez@gmail.com>
2025-03-09 13:24:24 +0200

    format with black

--- portal/admin.py
+++ portal/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from . models import *
+from .models import Guard
 
-admin.site.register(Guard)
+@admin.register(Guard)
+class GuardAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name', 'last_name', 'gender', 'identification_document', 'date_of_birth', 'age',
+        'cellphone_number', 'village_assigned', 'date_employed', 'status'
+    )
+    search_fields = ('first_name', 'last_name', 'identification_document')
+    list_filter = ('gender', 'status', 'village_assigned')
+    ordering = ('last_name',)
--- portal/models.py
+++ portal/models.py
@@ -1,28 +1,26 @@
 from django.db import models
-from django.conf import settings
 from django.core.validators import RegexValidator
 from datetime import date
 
 class Guard(models.Model):
-    
     SEX = [
-        ('male', 'Male'),
-        ('female', 'Female'),
+        ("male", "Male"),
+        ("female", "Female"),
     ]
     USER_STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('inactive', 'Inactive'),
+        ("active", "Active"),
+        ("inactive", "Inactive"),
     ]
-    VILLAGE_ASSIGNED =[
-        ('anabeb', 'Anabeb'),
-        ('otjondunda', 'Otjondunda'),
-        ('ombaikiha', 'Ombaikiha'),
-        ('warmquelle', 'Warmquelle'),
-        ('khowarib', 'Khowarib'),
-        ('omukutu', 'Omukutu'),
-        ('otjondumbu', 'Otjondumbu'),
-        ('okaturua', 'Okaturua'),
-        ('otjatjondjira', 'Otjatjondjira')
+    VILLAGE_ASSIGNED = [
+        ("anabeb", "Anabeb"),
+        ("otjondunda", "Otjondunda"),
+        ("ombaikiha", "Ombaikiha"),
+        ("warmquelle", "Warmquelle"),
+        ("khowarib", "Khowarib"),
+        ("omukutu", "Omukutu"),
+        ("otjondumbu", "Otjondumbu"),
+        ("okaturua", "Okaturua"),
+        ("otjatjondjira", "Otjatjondjira"),
     ]
 
     first_name = models.CharField(max_length=150, null=True, blank=True)
@@ -30,21 +28,33 @@ class Guard(models.Model):
     gender = models.CharField(max_length=10, choices=SEX, default="male")
     identification_document = models.CharField(
         max_length=15,
-        validators=[RegexValidator(r'^\d{1,15}$', 'Identification number must contain only digits and be up to 15 digits long.')]
+        validators=[
+            RegexValidator(
+                r"^\d{1,15}$",
+                "Identification number must contain only digits and be up to 15 digits long.",
+            )
+        ],
     )
     date_of_birth = models.DateField(null=True, blank=True)
     cellphone_number = models.CharField(
         max_length=15,
-        validators=[RegexValidator(r'^\d{1,15}$', 'Phone number must contain only digits and be up to 15 digits long.')]
+        validators=[
+            RegexValidator(
+                r"^\d{1,15}$",
+                "Phone number must contain only digits and be up to 15 digits long.",
+            )
+        ],
     )
-    village_assigned = models.CharField(max_length=50, choices=VILLAGE_ASSIGNED, default="warmquelle")
+    village_assigned = models.CharField(
+        max_length=50, choices=VILLAGE_ASSIGNED, default="warmquelle"
+    )
     date_employed = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    status = models.CharField(max_length=10, choices=USER_STATUS_CHOICES, default='active')
+    status = models.CharField(max_length=10, choices=USER_STATUS_CHOICES, default="active")
 
     def __str__(self):
         return self.first_name
-    
-    def calculate_age(self):
+
+    def age(self):
         if self.date_of_birth:
             today = date.today()
             age = today.year - self.date_of_birth.year
@@ -52,3 +62,5 @@ class Guard(models.Model):
                 age -= 1
             return age
         return None
+
+        
