855df1303c9bc60831d5cc3f251f51a6d6f0298a
Ngazetungue <nmuheuez@gmail.com>
2025-03-09 15:02:15 +0200

    add function to calculate membership period of members

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Guard
+from .models import Guard, Member
 
 @admin.register(Guard)
 class GuardAdmin(admin.ModelAdmin):
@@ -9,4 +9,14 @@ class GuardAdmin(admin.ModelAdmin):
     )
     search_fields = ('first_name', 'last_name', 'identification_document')
     list_filter = ('gender', 'status', 'village_assigned')
+    ordering = ('last_name',)
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name', 'last_name', 'gender', 'identification_document', 'date_of_birth', 'age', 
+         'date_join', 'status', 'date_deceased', 'membership_period', 'cellphone_number', 'village'
+    )
+    search_fields = ('first_name', 'last_name', 'identification_document')
+    list_filter = ('status', 'village')
     ordering = ('last_name',)
--- portal/models.py
+++ portal/models.py
@@ -7,7 +7,7 @@ class Guard(models.Model):
         ("male", "Male"),
         ("female", "Female"),
     ]
-    USER_STATUS_CHOICES = [
+    GUARD_STATUS_CHOICES = [
         ("active", "Active"),
         ("inactive", "Inactive"),
     ]
@@ -23,25 +23,25 @@ class Guard(models.Model):
         ("otjatjondjira", "Otjatjondjira"),
     ]
 
-    first_name = models.CharField(max_length=150, null=True, blank=True)
-    last_name = models.CharField(max_length=150, null=True, blank=True)
+    first_name = models.CharField(max_length=150, null=False, blank=False)
+    last_name = models.CharField(max_length=150, null=False, blank=False)
     gender = models.CharField(max_length=10, choices=SEX, default="male")
     identification_document = models.CharField(
         max_length=15,
         validators=[
             RegexValidator(
-                r"^\d{1,15}$",
-                "Identification number must contain only digits and be up to 15 digits long.",
+                r"^\d{1,11}$",
+                "Identification number must contain only digits and be up to 11 digits long.",
             )
         ],
     )
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=False, blank=False)
     cellphone_number = models.CharField(
         max_length=15,
         validators=[
             RegexValidator(
                 r"^\d{1,15}$",
-                "Phone number must contain only digits and be up to 15 digits long.",
+                "Phone number must contain only digits and be up to 10 digits long.",
             )
         ],
     )
@@ -49,7 +49,7 @@ class Guard(models.Model):
         max_length=50, choices=VILLAGE_ASSIGNED, default="warmquelle"
     )
     date_employed = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    status = models.CharField(max_length=10, choices=USER_STATUS_CHOICES, default="active")
+    status = models.CharField(max_length=10, choices=GUARD_STATUS_CHOICES, default="alive")
 
     def __str__(self):
         return self.first_name
@@ -63,4 +63,76 @@ class Guard(models.Model):
             return age
         return None
 
-        
+class Member(models.Model):
+    SEX = [
+        ("male", "Male"),
+        ("female", "Female"),
+    ]
+    MEMBER_STATUS_CHOICES = [
+        ("alive", "Alive"),
+        ("deceased", "Deceased"),
+    ]
+    VILLAGE = [
+        ("anabeb", "Anabeb"),
+        ("otjondunda", "Otjondunda"),
+        ("ombaikiha", "Ombaikiha"),
+        ("warmquelle", "Warmquelle"),
+        ("khowarib", "Khowarib"),
+        ("omukutu", "Omukutu"),
+        ("otjondumbu", "Otjondumbu"),
+        ("okaturua", "Okaturua"),
+        ("otjatjondjira", "Otjatjondjira"),
+    ]
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    gender = models.CharField(max_length=10, choices=SEX, default="male")
+    identification_document = models.CharField(
+        max_length=15,
+        validators=[
+            RegexValidator(
+                r"^\d{1,11}$",
+                "Identification number must contain only digits and be up to 11 digits long.",
+            )
+        ],
+    )
+    date_of_birth = models.DateField()
+    cellphone_number = models.CharField(
+        max_length=15,
+        validators=[
+            RegexValidator(
+                r"^\d{1,10}$",
+                "Phone number must contain only digits and be up to 10 digits long.",
+            )
+        ],
+    )
+    village = models.CharField(
+        max_length=50, choices=VILLAGE, default="warmquelle"
+    )
+    date_join = models.DateField()
+    status = models.CharField(max_length=10, choices=MEMBER_STATUS_CHOICES, default="alive")
+    date_deceased = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def age(self):
+        if self.date_of_birth:
+            today = date.today()
+            age = today.year - self.date_of_birth.year
+            if (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day):
+                age -= 1
+            return age
+        return None
+
+    def membership_period(self):
+        if self.status == "deceased" and self.date_deceased:
+            end_date = self.date_deceased
+        else:
+            end_date = date.today()
+        active_years = end_date.year - self.date_join.year
+
+        if (end_date.month, end_date.day) < (self.date_join.month, self.date_join.day):
+            active_years -= 1
+        return active_years
+
